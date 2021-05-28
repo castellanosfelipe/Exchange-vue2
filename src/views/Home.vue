a54cbf4b812acf745403b03b2225b4452fe54a73
Felipe <ing.felipe55@outlook.com>
2021-05-27 21:35:14 -0500

    Exchange

---
+++ src/views/Home.vue
@@ -0,0 +1,30 @@
+<template>
+   <div>
+      <bounce-loader :loading="isLoading" :color="'#68d391'" :size="100" />
+      <px-asset-table v-if="!isLoading" :assets="assets" />
+   </div>
+</template>
+<script>
+import api from '@/api'
+import PxAssetTable from '@/components/PxAssetTable';
+
+export default {
+   name: 'Home',
+   components: {
+      PxAssetTable
+   },
+   data() {
+      return {
+         assets: [],
+         isLoading: false,
+      }
+   },
+   created() {
+      this.isLoading = true;
+
+      api.getAssets()
+         .then(assets => this.assets = assets) 
+         .finally(() => this.isLoading = false)
+   },
+}
+</script>
